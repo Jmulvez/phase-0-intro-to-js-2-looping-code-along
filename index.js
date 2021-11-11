a35e03f206b51fe8662b16efd27b73ba438ceed7
John <johnmulvey48@yahoo.com>
2021-11-11 09:24:40 -0500

    git@github.com:Jmulvez/phase-0-intro-to-js-2-looping-code-along.git

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+  I'm 30 years old. Happy birthday to me!
+  I'm 31 years old. Happy birthday to me!
+  I'm 32 years old. Happy birthday to me!
+  I'm 33 years old. Happy birthday to me!
+  I'm 34 years old. Happy birthday to me!
+  I'm 35 years old. Happy birthday to me!
+  I'm 36 years old. Happy birthday to me!
+  I'm 37 years old. Happy birthday to me!
+  I'm 38 years old. Happy birthday to me!
+  I'm 39 years old. Happy birthday to me!
